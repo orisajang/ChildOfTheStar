e4c106cbf1f5ce44d5a1bd781895e2660de0b55c
orisajang <white928620@gmail.com>
2025-12-13 21:21:22 +0900

    [feat] 타일 이동해야할경우 Controll가 Model에 어떤 값을 줘야할지 고려해서 추가
    View에서 값을 주면, controller가 이벤트 형식으로 Model에 줄 수 있도록 메서드 추가

--- Assets/Scripts/TileBoardController.cs
+++ Assets/Scripts/TileBoardController.cs
@@ -1,5 +1,7 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
+using static BoardModel;
 
 public class TileBoardController : MonoBehaviour
 {
@@ -8,7 +10,7 @@ public class TileBoardController : MonoBehaviour
     //3.Model의 이동이 정상적으로 되었다면?
     //- 성공하면 View에게 어떻게 변하라고 전달 (이동만 해라, 매치 발생하면? Model에서 변한 배열을 View에 넘겨줘서 View에서 처리하게?? 
     //- 실패하면 View에게 변하지말라고 전달
-    TileBoardModelTest modelTest = new TileBoardModelTest();
+    BoardModel tileBoardModel = new BoardModel();
     TileBoardView tileBoardView;
 
     //입력을 위해서 정의
@@ -18,6 +20,10 @@ public class TileBoardController : MonoBehaviour
     InputAction _pointAction;
     //클릭중인지
     bool _isDragging;
+
+    //이벤트
+    //이동을 얼마나 해야하는지 
+    public event Action<TileMoveDirection, int, int> OnMoveTile;
 
     private void Awake()
     {
@@ -31,16 +37,17 @@ public class TileBoardController : MonoBehaviour
     {
         //추후 추가예정(view에서 타일 이동이 끝났을떄?)
         //tileBoardView.OnTileMoved += OnTileMoveTriggered;
-        modelTest.OnMoveSuccessed += OnTileMoveSuccess;
         _uiAction.started += OnMouseClickStart;
         _uiAction.canceled += OnMouseClickEnd;
+
     }
     private void OnDisable()
     {
         //tileBoardView.OnTileMoved -= OnTileMoveTriggered;
-        modelTest.OnMoveSuccessed -= OnTileMoveSuccess;
         _uiAction.started -= OnMouseClickStart;
         _uiAction.canceled -= OnMouseClickEnd;
+
+
     }
     private void Update()
     {
@@ -51,28 +58,16 @@ public class TileBoardController : MonoBehaviour
             //Debug.Log("클릭후 드래그중 " + screenPos);
         }
     }
-    private void OnTileMoveTriggered(int pos, int moveValue)
-    {
-        modelTest.MoveTile(pos, moveValue);
-    }
     /// <summary>
-    /// 타일 이동이 정상적으로 완료되었는지
+    /// VIew에서 정보를 받아서 model에 전달 
     /// </summary>
-    /// <param name="isSuccess"></param>
-    private void OnTileMoveSuccess(bool isSuccess, int[,] tileInfo)
+    /// <param name="direction">가로,세로 방향중 어디로 이동중인지</param>
+    /// <param name="currentLine">현재 몇번째 가로/세로 줄을 선택중인지 </param>
+    /// <param name="moveValue">얼마나 이동시켜야하는지 </param>
+    private void OnTileMoveTriggered(TileMoveDirection direction, int currentLine, int moveValue)
     {
-        //성공과 실패 처리
-        if (isSuccess)
-        {
-            //성공했다면? View를 바꾸는 행동 진행
-            //View에 바뀐 타일 정보를 보내주면, 그 정보를 토대로 진행
-            //int[,] tiles = new int[6, 5];
-            //tileBoardView.SetTileInfoUI(tileInfo);
-        }
-        else
-        {
-            //실패했다면? View에서 아무것도 안함
-        }
+        //modelTest.MoveTile(pos, moveValue);
+        OnMoveTile?.Invoke(direction, currentLine, moveValue);
     }
     private void OnMouseClickStart(InputAction.CallbackContext ctx)
     {
--- Assets/Scripts/TileBoardModelTest.cs
+++ Assets/Scripts/TileBoardModelTest.cs
@@ -2,23 +2,6 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// Model임시용(그냥 있다고 가정하기 위해서)
-/// </summary>
-public class TileBoardModelTest
-{
-    public event Action<bool, int[,]> OnMoveSuccessed;
-    int[,] tileArray = new int[6, 5];
-    //실제 타일 이동 로직을 처리
-    public void MoveTile(int a, int b)
-    {
-        //타일 이동하고, 3매치되면 터지고, 다시채우는 로직작성
-        //성공하면 이벤트 발동
-        OnMoveSuccessed?.Invoke(true, tileArray);
-        //실패하면 이벤트 발동
-        //OnMoveSuccessed?.Invoke(false, tileArray);
-    }
-}
 
 
 
