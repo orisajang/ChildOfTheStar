528d308b18015b758815b48212b3977752ad35bc
orisajang <white928620@gmail.com>
2025-12-16 02:11:22 +0900

    [feat] 타일보드 Controller 이동할때 마우스 좌표를 View에 보내도록 추가
    이렇게 보내는게 맞는지 확인 후 사용

--- Assets/Scripts/Tile/CreateTilePoint.cs
+++ Assets/Scripts/Tile/CreateTilePoint.cs
@@ -1,3 +1,4 @@
+using Unity.Hierarchy;
 using UnityEngine;
 
 public class CreateTilePoint : MonoBehaviour
@@ -7,10 +8,18 @@ public class CreateTilePoint : MonoBehaviour
     private float _tileBoardWith;
     private float _tileBoardHeight;
 
-    private float _tileGapX;
-    private float _tileGapY;
+    public float _tileGapX { get; private set; }
+    public float _tileGapY { get; private set; }
 
     private SpriteRenderer tileBoardSprite;
+
+    //TileBoardController.cs 파일에서 사용하기 위해서 정의
+    //보드 안쪽에 실제 배치된 타일들의 위치 계산
+    public Vector2[,] tempPointPosition { get; private set; }
+    //타일의 시작점, 끝점을 저장
+    public Vector2 standardTilePositionStart { get; private set; }
+    public Vector2 standardTilePositionEnd { get; private set; }
+
     private void Awake()
     {
         tileBoardSprite = GetComponent<SpriteRenderer>();
@@ -23,6 +32,11 @@ public class CreateTilePoint : MonoBehaviour
 
         var standardPosition = new Vector2(transform.position.x - (_tileBoardWith / 2), transform.position.y + (_tileBoardHeight / 2));
         GameObject tempPoint;
+
+        //포지션의 위치를 지정하기 위해서 추가
+        tempPointPosition = new Vector2[_tileBoardSize.xSize, _tileBoardSize.ySize];
+
+
         //특정 위치에 point용 게임 오브젝트 생성
         for (int i = 1; i<= _tileBoardSize.ySize; i++)
         {
@@ -31,8 +45,40 @@ public class CreateTilePoint : MonoBehaviour
                 tempPoint = new GameObject($"point{i}_{j}");
                 tempPoint.transform.parent = transform;
                 tempPoint.transform.position = standardPosition + new Vector2(j * _tileGapX, (-i) * _tileGapY);
+
+                //TileBoardController에서 사용하기위해 좌표, 디버그용 라인추가
+                //좌표 추가
+                tempPointPosition[j - 1, i - 1] = tempPoint.transform.position;
+                //디버그용 라인 표시 (주석 해제하면 런타임중에 타일 영역 보임
+                float x1 = tempPoint.transform.position.x - (_tileGapX / 2);
+                float x2 = tempPoint.transform.position.x + (_tileGapX / 2);
+                float y1 = tempPoint.transform.position.y - (_tileGapY / 2);
+                float y2 = tempPoint.transform.position.y + (_tileGapY / 2);
+                LineRenderer lr = tempPoint.AddComponent<LineRenderer>();
+                lr.positionCount = 5;
+                lr.loop = false;
+                lr.useWorldSpace = true;
+                lr.widthMultiplier = 0.05f;
+                lr.material = new Material(Shader.Find("Sprites/Default"));
+                lr.startColor = Color.red;
+                lr.endColor = Color.red;
+                tempPoint.transform.localScale = Vector3.one;
+                lr.SetPosition(0, new Vector2(x1, y1));
+                lr.SetPosition(1, new Vector2(x2, y1));
+                lr.SetPosition(2, new Vector2(x2, y2));
+                lr.SetPosition(3, new Vector2(x1, y2));
+                lr.SetPosition(4, new Vector2(x1, y1));
             }
         }
+        //계산 끝난뒤에 처음시작하는 타일의 위치점과 끝점을 계산
+        //1. 타일에서 왼쪽위의 지점
+        float tileStartX = tempPointPosition[0, 0].x - (_tileGapX/2);
+        float tileStartY = tempPointPosition[0, 0].y + (_tileGapY/2);
+        standardTilePositionStart = new Vector2(tileStartX, tileStartY);
+        //2. 타일에서 오른쪽 아래의 지점
+        float tileEndX = tempPointPosition[_tileBoardSize.xSize-1, _tileBoardSize.ySize-1].x + (_tileGapX/2);
+        float tileEndY = tempPointPosition[_tileBoardSize.xSize-1, _tileBoardSize.ySize-1].y - (_tileGapY/2);
+        standardTilePositionEnd = new Vector2(tileEndX, tileEndY);
     }
 
 }
--- Assets/Scripts/TileBoardController.cs
+++ Assets/Scripts/TileBoardController.cs
@@ -18,12 +18,23 @@ public class TileBoardController : MonoBehaviour
     InputAction _uiAction;
     //클릭후 드래그중인지 알기위해서 사용
     InputAction _pointAction;
-    //클릭중인지
-    bool _isDragging;
+    
+    // 방향 판단 임계값 이 값 이상 움직여여야지 방향 결정 수치 변경 예정----------------------------------------------------------
+    [SerializeField] private float positionThreshold = 1f;
 
     //이벤트
     //이동을 얼마나 해야하는지 
     public event Action<TileMoveDirection, int, int> OnMoveTile;
+
+    //시작 관련
+    bool _isDragging;
+    Vector2 startPos;
+    // 상하 또는 좌우 고정 여부
+    private bool _isMoveFix;
+    // 상하
+    private bool _isUpDown;
+    // 좌우
+    private bool _isLeftRight;
 
     private void Awake()
     {
@@ -51,11 +62,48 @@ public class TileBoardController : MonoBehaviour
     }
     private void Update()
     {
+        //업데이트 수정 필요할듯? -> 클릭할때마다 View에서 보이도록, model에 데이터 전송할 수있도록?
         if (_isDragging)
         {
             Vector2 screenPos = _pointAction.ReadValue<Vector2>();
-            tileBoardView.UpdateDrag(screenPos);
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+            tileBoardView.UpdateDrag(worldPos);
             //Debug.Log("클릭후 드래그중 " + screenPos);
+
+
+            // 이동 거리 계산
+            Vector2 changePosition = worldPos - startPos;
+
+            if (_isMoveFix == false)
+            {
+                // 임계값을 넘었을 때만 방향 결정
+                if (Mathf.Abs(changePosition.x) > positionThreshold || Mathf.Abs(changePosition.y) > positionThreshold)
+                {
+                    // 좌우 이동이 더 클 때
+                    if (Mathf.Abs(changePosition.x) > Mathf.Abs(changePosition.y))
+                    {
+                        _isLeftRight = true;
+                        _isUpDown = false;
+                    }
+                    // 상하 이동이 더 클 때
+                    else if (Mathf.Abs(changePosition.x) < Mathf.Abs(changePosition.y))
+                    {
+                        _isUpDown = true;
+                        _isLeftRight = false;
+                    }
+                    _isMoveFix = true;
+                }
+            }
+            // 좌우 이동이 더 크니 상하 이동값 무시
+            if (_isLeftRight == true)
+            {
+                //return new Vector2(changePosition.x, 0);
+            }
+            // 상하 이동이 더 크니 좌우 이동값 무시
+            if (_isUpDown == true)
+            {
+                //return new Vector2(0, changePosition.y);
+            }
         }
     }
     /// <summary>
@@ -69,19 +117,145 @@ public class TileBoardController : MonoBehaviour
         //modelTest.MoveTile(pos, moveValue);
         OnMoveTile?.Invoke(direction, currentLine, moveValue);
     }
+    [SerializeField] CreateTilePoint tilePoint;
     private void OnMouseClickStart(InputAction.CallbackContext ctx)
     {
-        _isDragging = true;
+        // 상태 초기화
+        _isMoveFix = false;
+        _isUpDown = false;
+        _isLeftRight = false;
         Vector2 screenPos = _pointAction.ReadValue<Vector2>();
-        tileBoardView.Drag(screenPos);
         //Debug.Log("클릭시작 " + screenPos);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        //시작한 좌표 저장
+        startPos = worldPos;
+
+        //1. 클릭한 영역이 타일영역인지 확인
+        //타일 바깥을 클릭한 경우 아무것도 하지않음 (타일의 왼쪽, 오른쪽 위, 아래쪽 체크)
+        if (worldPos.x < tilePoint.standardTilePositionStart.x)
+        {
+            return;
+        }
+        if (worldPos.x > tilePoint.standardTilePositionEnd.x)
+        {
+            return;
+        }
+        if (worldPos.y > tilePoint.standardTilePositionStart.y)
+        {
+            return;
+        }
+        if (worldPos.y < tilePoint.standardTilePositionEnd.y)
+        {
+            return;
+        }
+
+        //Debug.Log("성공");
+        _isDragging = true;
+
+        //스탠다드 포지션과, 타일갭x, 타일갭y를 통해 처리
+        //tilePoint.standardPosition;
+        //tilePoint._tileGapX;
+        //tilePoint._tileGapY;
+        //둘의 값을 뺀다, tileGapx만큼 나눈다. 그럼 몇번째 X인지 체크 가능
+        //tileBoard에서 그냥 좌표를 가지고있게 하자 (tempPointPosition)
+        int xPos = (int)((worldPos.x - tilePoint.standardTilePositionStart.x) / tilePoint._tileGapX);
+        int yPos = (int)((tilePoint.standardTilePositionStart.y - worldPos.y) / tilePoint._tileGapY);
+        //선택된 타일의 좌표를 보낸다
+        Vector2 selectedPos = tilePoint.tempPointPosition[xPos, yPos];
+        tileBoardView.Drag(selectedPos);
+        Debug.Log($"클릭시작. 좌표= {xPos}, {yPos}");
     }
     private void OnMouseClickEnd(InputAction.CallbackContext ctx)
     {
+        //드래깅중일때만 End 동작
+        if (!_isDragging) return;
+        //입력 좌표를 가져옴
         _isDragging = false;
         Vector2 screenPos = _pointAction.ReadValue<Vector2>();
-        tileBoardView.EndDrag(screenPos);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
         //Debug.Log("클릭끝 " + screenPos);
+
+        //마우스를 뗀 좌표가 좌표의 어느 지점인지를 보내주면 된다.
+        //상하좌우 지점의 좌표를 벗어낫을때는 Gap값을 이용해서 좌표를 변환해준다
+
+        //정상작동인지 체크위해 isFixed 선언
+        bool isFixed = false;
+        //좌표에서 X축과 Y축을 보내기 위해 선언
+        int xPos = 0;
+        int yPos = 0;
+        //타일의 x축길이, y축 길이
+        int xLength = tilePoint.tempPointPosition.GetLength(0);
+        int yLength = tilePoint.tempPointPosition.GetLength(1);
+        //예외처리 (if문 4개) 타일 바깥에서 마우스 해제한경우
+        //x왼쪽 방향
+        if (worldPos.x < tilePoint.standardTilePositionStart.x)
+        {
+            isFixed = true;
+            float k = tilePoint.standardTilePositionStart.x - worldPos.x;
+            //왼쪽으로 몇번이나 초과해서 이동했는지
+            int a1 = (int)(k / tilePoint._tileGapX);
+            //x축가로 최대 6개이므로 0~5까지 범위로만 되도록 나머지 연산
+            int a2 = a1 % xLength;
+            xPos = (xLength-1) - a2;
+        }
+        if (worldPos.x > tilePoint.standardTilePositionEnd.x)
+        {
+            isFixed = true;
+            float k = worldPos.x - tilePoint.standardTilePositionEnd.x;
+            //왼쪽으로 몇번이나 초과해서 이동했는지
+            int a1 = ((int)(k / tilePoint._tileGapX));
+            //x축가로 최대 6개이므로 0~5까지 범위로만 되도록 나머지 연산
+            int a2 = a1 % xLength;
+            xPos = a2;
+        }
+        if (worldPos.y > tilePoint.standardTilePositionStart.y)
+        {
+            isFixed = true;
+            float k = worldPos.y - tilePoint.standardTilePositionStart.y;
+            //왼쪽으로 몇번이나 초과해서 이동했는지
+            int a1 = (int)(k / tilePoint._tileGapY);
+            //y축세로 최대 5개이므로 0~4까지 범위로만 되도록 나머지 연산
+            int a2 = a1 % yLength;
+            yPos = (yLength-1) - a2;
+        }
+        if (worldPos.y < tilePoint.standardTilePositionEnd.y)
+        {
+            isFixed = true;
+            float k = tilePoint.standardTilePositionEnd.y - worldPos.y;
+            //왼쪽으로 몇번이나 초과해서 이동했는지
+            int a1 = ((int)(k / tilePoint._tileGapY));
+            //y축세로 최대 5개이므로 0~4까지 범위로만 되도록 나머지 연산
+            int a2 = a1 % yLength;
+            yPos = a2;
+        }
+        //보드 범위를 초과해서 마우스가 이동한 경우 반대좌표를 강제로 넣어준다
+        if(isFixed)
+        {
+            if(_isLeftRight)
+            {
+                yPos = (int)((tilePoint.standardTilePositionStart.y - worldPos.y) / tilePoint._tileGapY);
+            }
+            else if (_isUpDown)
+            {
+                xPos = (int)((worldPos.x - tilePoint.standardTilePositionStart.x) / tilePoint._tileGapX);
+            }
+        }
+
+        //타일 범위 안넘어가고 타일안에서 마우스 작동한 경우는 특별한 예외처리없이 그냥 현재 마우스좌표로 좌표를 계산
+        if(!isFixed)
+        {
+            xPos = (int)((worldPos.x - tilePoint.standardTilePositionStart.x) / tilePoint._tileGapX);
+            yPos = (int)((tilePoint.standardTilePositionStart.y - worldPos.y) / tilePoint._tileGapY);
+            
+        }
+        //결과 출력
+        Debug.Log($"클릭끝. 좌표= {xPos}, {yPos}");
+        Vector2 selectedPos = tilePoint.tempPointPosition[xPos, yPos];
+        //선택된 타일의 좌표를 보낸다
+        tileBoardView.EndDrag(selectedPos);
+
+
     }
 
 
