6343368b0a71e5abc27de54fa4a1aafcc4df27f0
orisajang <white928620@gmail.com>
2025-12-17 01:08:37 +0900

    [feat] Player 정보도 CSV에서 읽어오도록 추가

---
+++ Assets/Scripts/CSV/PlayerCSVData.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+public struct PlayerCSVData
+{
+    //CSV파일에 저장되어있는 속성들
+    public int id;
+    public string name;
+    public int startDeckId;
+    public int charHP;
+    public int movePoint;
+    public string sprite;
+    public string animation;
+    public string sound;
+}
