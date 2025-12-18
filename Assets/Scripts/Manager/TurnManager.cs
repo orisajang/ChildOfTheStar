9d273351fe1b4255ffc6050ac9ea749874c9bf99
mibana <ksy661004@gmail.com>
2025-12-18 11:09:37 +0900

    [Feat] 불필요한 부분 삭제 및 보드 활성화 비 활성화 추가

--- Assets/Scripts/Manager/TurnManager.cs
+++ Assets/Scripts/Manager/TurnManager.cs
@@ -5,27 +5,15 @@ public class TurnManager : Singleton<TurnManager>
 {
     public enum Turn
     {
-        Nune,
+        None,
         PlayerTurn,
         MonsterTurn,
-        StageTurn
     }
     public Turn CurrentTurn { get; private set; }
 
+    BoardBlock boardBlock;
+
     private Player player;
-
-    //[SerializeField] private TileBoardView tileBoardView;
-
-    // 나중에 Player에서 가져오게 바꾸기
-    int _characterHpCurrent;
-    int _MovementPointMax;
-    int _MovementPointCurrent;
-
-    // 나중에 monstermanager에서 가져오기
-    int _monsterHp;
-    int _monsterMaxEnergy;
-    int _monsterPointCurrent;
-
 
     private void OnEnable()
     {
@@ -46,24 +34,20 @@ public class TurnManager : Singleton<TurnManager>
         if(PlayerManager.Instance._player._characterHpCurrent <= 0)
         {
             Debug.Log("플레이어 사망 상태입니다.");
-            CurrentTurn = Turn.Nune;
+            CurrentTurn = Turn.None;
             return;
         }
         else if (PlayerManager.Instance._player._characterHpCurrent > 0)
         {
             CurrentTurn = Turn.PlayerTurn;
 
-            // player에서 가져오면 그 때 수정
             //플레이어 행동력을 최대 행동력값으로
             PlayerManager.Instance._player.SetMovementPointInit();
-            _MovementPointCurrent = PlayerManager.Instance._player._MovementPointCurrent;
 
 
             //타겟 몬스터 지정 (추가) -플레이어가 타겟 몬스터를 지정할때까지 기다려야함
             //게임뷰에서 몬스터를 클릭하면 그 이후부터 타일 활성화 (StartPlayerBoardActive 메서드 실행)
             MonsterManager.Instance.EnableSelectMonsterTarget();
-            //tileBoardView.SetBoardActive(true);
-            //Debug.Log("플레이어 턴 시작");
         }
     }
     public void StartPlayerBoardActive()
@@ -71,7 +55,10 @@ public class TurnManager : Singleton<TurnManager>
         //마우스클릭기능 비활성화 하고
         MonsterManager.Instance.DisableSelectMonsterTarget();
         //이후 타일 이동 시작
-        //tileBoardView.SetBoardActive(true);
+
+        // 보드에서 막 비활성화
+        boardBlock.SetBoardActive(false);
+
         Debug.Log("플레이어 턴 시작");
     }
 
@@ -83,15 +70,16 @@ public class TurnManager : Singleton<TurnManager>
     {
         if (PlayerManager.Instance._player._MovementPointCurrent <= 0)
         {
-            //tileBoardView.SetBoardActive(false);
-
             Debug.Log("플레이어 턴 종료");
+
+            boardBlock.SetBoardActive(true);
 
             StartMonsterTurn();
         }
         else if(PlayerManager.Instance._player._MovementPointCurrent > 0)
         {
             Debug.Log("이동력이 남아있습니다.");
+            return;
         }
     }
 
@@ -101,21 +89,16 @@ public class TurnManager : Singleton<TurnManager>
     private void StartMonsterTurn()
     {
         //몬스터 갯수가 0개 이상이라면 몬스터 존재 
-        //if (_monsterHp <= 0)
         if(MonsterManager.Instance.SpawnedMonster.Count == 0)
         {
             Debug.Log("모든 몬스터 사망 상태입니다.");
-            CurrentTurn = Turn.Nune;
+            CurrentTurn = Turn.None;
             return;
         }
-        //else if (_monsterHp > 0)
         else if(MonsterManager.Instance.SpawnedMonster.Count > 0)
         {
             // 몬스터 턴으로 변경
             CurrentTurn = Turn.MonsterTurn;
-
-            // 몬스터 이동력 채우기
-            //_monsterPointCurrent = _monsterMaxEnergy;
 
             //보유한 몬스터를 전부 전투 처리
             MonsterManager.Instance.StartMonsterAction();
