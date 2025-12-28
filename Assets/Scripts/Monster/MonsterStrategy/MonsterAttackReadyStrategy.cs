782ce7cedde06af4b675b861d4c41a18a358cb20
orisajang <white928620@gmail.com>
2025-12-29 00:38:35 +0900

    [feat] 몬스터 행동별 소리 재생

--- Assets/Scripts/Monster/MonsterStrategy/MonsterAttackReadyStrategy.cs
+++ Assets/Scripts/Monster/MonsterStrategy/MonsterAttackReadyStrategy.cs
@@ -6,6 +6,9 @@ public class MonsterAttackReadyStrategy : MonsterStrategy
     {
         //현재 아무것도안함. 추후 sprite이미지, sound, effect만 변경되면 될듯 
         Debug.Log("AttackReadyAct");
+        //애니메이션 재생
         monster.MonsterAnimatorChange(MonsterAnimatorParameterName.AttackReady);
+        //소리 재생
+        SoundManager.Instance.PlayEffect(action.monsterActionData.sound);
     }
 }
