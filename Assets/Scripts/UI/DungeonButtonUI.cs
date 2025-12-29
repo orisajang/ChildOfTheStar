bc650a2172039cfb13bffd7baa93462f93ddf71b
orisajang <white928620@gmail.com>
2025-12-29 18:14:05 +0900

    [feat] Lobby에 리소스 적용

--- Assets/Scripts/UI/DungeonButtonUI.cs
+++ Assets/Scripts/UI/DungeonButtonUI.cs
@@ -6,6 +6,7 @@ public class DungeonButton : MonoBehaviour
     [SerializeField] private GameObject dungeonButton;
     //[SerializeField] private GameObject characterButton;
     [SerializeField] private GameObject panel;
+    [SerializeField] SpriteRenderer backgroundImage;
 
 
     public void Open()
@@ -13,6 +14,8 @@ public class DungeonButton : MonoBehaviour
         lobbyButton.SetActive(true);
         panel.SetActive(true);
         dungeonButton.SetActive(false);
+
+        backgroundImage.sprite = Resources.Load<Sprite>("Image/" + "selectstage_bg");
 
         //RectTransform rect = characterButton.GetComponent<RectTransform>();
         //rect.anchoredPosition = new Vector2(112f, 28f);
