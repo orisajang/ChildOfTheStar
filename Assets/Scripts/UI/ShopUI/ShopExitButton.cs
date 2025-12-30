f86751cd7af0f0268fca6de0b31cf705ef9b6288
mibana <ksy661004@gmail.com>
2025-12-30 19:48:11 +0900

    [fix] 상점 퇴장 후에도 배경음 안 바뀜 수정

--- Assets/Scripts/UI/ShopUI/ShopExitButton.cs
+++ Assets/Scripts/UI/ShopUI/ShopExitButton.cs
@@ -4,6 +4,7 @@ public class ShopExitButton : MonoBehaviour
 {
     public void OnShopExit()
     {
+        DungeonManager.Instance.RePlayCurrentDungeonBGM();
         //Debug.Log("상점 나가기 버튼 클릭됨");
         GameManager.Instance.GoToStageScene();
     }
